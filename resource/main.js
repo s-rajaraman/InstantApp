b2fc016ee10f92e0870784994573b4e145a6f370
Sriram Rajaraman <S.Rajaraman89@gmail.com>
2016-08-20 19:30:48 -0400

    Basic evernote app

---
+++ resource/main.js
@@ -0,0 +1,27 @@
+//Got code from https://github.com/electron/electron/blob/master/docs/tutorial/quick-start.md
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 800, height:600, webPreferences: {nodeIntegration: false}});
+  win.loadURL('https://www.evernote.com/')
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
