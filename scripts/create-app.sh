150a0f2f835a2039507b87ae981e4ca9427d0714
Sriram Rajaraman <S.Rajaraman89@gmail.com>
2016-08-21 09:49:08 -0400

    Server creates app; can be downloaded

--- scripts/create-app.sh
+++ scripts/create-app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
 url=$1
 name=$2
 platform=$3
 arch=$4
+uuid=$5
 dir=../dist/
-uuid=$(uuid -v 4)
+
 tmpdir=../tmp/$uuid/
 resource=../resource
 
@@ -20,11 +20,13 @@ if curl --output /dev/null --silent --head --fail "$url"; then
   electron-packager $tmpdir $name --platform=$platform --arch=$arch --out=$dir --version=1.3.3 --overwrite
   rm -r $tmpdir
 else
+  echo '1'
   exit 1
 fi
 
 if [ ! -d $dir/$name-$platform-$arch ]; then
+  echo '2'
   exit 2
 fi
-
+echo '3'
 exit 0
--- src/server.rb
+++ src/server.rb
@@ -1,10 +1,99 @@
 require 'sinatra'
+require 'tempfile'
+require "net/http"
+require 'fileutils'
+require 'uri'
+require "cgi"
+require 'digest'
+
+configure do
+  enable :logging
+end
+
+FileUtils::mkdir_p('dist')
+FileUtils::touch('dist/out.txt')
 
 get '/' do
-  send_file '../public/app/index.html'
+  send_file '../public/index.html'
+end
+
+get '/download' do
+  log "in /download"
+  platform, arch = params[:os].split('-')
+  url = params[:url]
+  name = params[:name]
+
+  log url
+  log platform
+
+  file_path = create_app url, name, platform, arch
+  log file_path
+  send_file file_path
 end
 
 
-get '/download/:name/:url/:os' do
-  send_file '../public/app/index.html'
+
+get '/check' do
+  url = params[:url]
+  log url
+  filename ="./dist/#{url}"
+  log "Downloading #{filename}"
+  send_file(filename, :type => "application/zip")
+end
+
+
+
+
+def create_app(url, name, platform, arch)
+  if url_invalid? url
+    return "ERROR #{url}"
+  end
+
+  tmp_folder_path = setup_tmp_dir()
+
+  find_and_replace_in_file("#{tmp_folder_path}/package.json", 'your-app', name)
+  find_and_replace_in_file("#{tmp_folder_path}/main.js", 'https://www.example.com/', url)
+
+  # replacement is to make sure the url http://google.com does not create a two directories
+  url_hash = Digest::hexencode url
+  log url_hash
+  output_dir = "./dist/#{url_hash}"
+  log output_dir
+  electron_app_folder_name = "#{output_dir}/#{name}-#{platform}-#{arch}"
+
+  output = `electron-packager #{tmp_folder_path} #{name} --platform=#{platform} --arch=#{arch} --out=#{output_dir} --version=1.3.3 --overwrite`
+  output = `zip -rqy #{electron_app_folder_name}.zip #{electron_app_folder_name} >> dist/out.txt`
+
+
+  #TODO: some thread should take care of the deleting
+  FileUtils::rm_r tmp_folder_path
+  FileUtils::rm_r electron_app_folder_name
+
+  return "#{electron_app_folder_name}.zip"
+end
+
+def url_invalid?(url)
+  output = `curl -s -m 5 --head #{url} | head -n 1`
+  return output.empty? || output.include?("404")
+end
+
+def find_and_replace_in_file(filepath, word, replacement_word)
+  text = File.read(filepath)
+  text = text.gsub(word, replacement_word)
+  File.open(filepath, "w") do |f|
+    f.write(text)
+  end
+end
+
+def setup_tmp_dir
+  uuid_folder = SecureRandom.uuid
+  tmp_folder_path = "../tmp/#{uuid_folder}"
+  FileUtils::mkdir_p "#{tmp_folder_path}"
+  FileUtils::cp_r '../resource/.', "#{tmp_folder_path}"
+  return tmp_folder_path
+end
+
+
+def log(text)
+  `echo #{text} >> dist/out.txt`
 end
